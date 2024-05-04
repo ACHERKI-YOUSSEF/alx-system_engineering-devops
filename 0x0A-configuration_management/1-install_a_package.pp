2dc36b49509291c3d671094d535bc78b520a04b4
YOUSSEF <youssef.acherki@um5r.ac.ma>
2024-05-03 18:25:42 -0700

    m

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,7 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-# Define a class for clarity
-class { 'install_flask':
-# Ensure pip3 is available (adjust based on your system)
-package { 'python3-pip': ensure => installed }
+#!/usr/bin/env pup
 
-# Install Flask with specific version using pip provider
-package { 'Flask':
-ensure => installed,
-provider => 'pip',
-require => Package['python3-pip'],  # Ensure pip is installed first
-# Specify the exact version
-# You can use a comparison operator like '<= 2.1.0' for a range
-version => '2.1.0',
-}
+# Install an specific version of flask (2.1.0)
+package { 'flask':
+  ensure  => '2.1.0',
+  provider => 'pip3',
 }
